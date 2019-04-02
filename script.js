b607bbba0b9d64703c872b4cd7d2cd0663d63eb1
Askie Lin <isme408@gmail.com>
2019-04-02 23:39:32 +0800

    let the display state of the clicked card turn to show

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this; // this => the clicked card
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
