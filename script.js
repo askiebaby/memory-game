17b35e716a6e55aa68295ba8f49fbbca31466673
Askie Lin <isme408@gmail.com>
2019-04-02 23:53:48 +0800

    prevented any card flipping before the cards are hidden or match

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  // 剛剛沒配對成功的話，就把牌蓋起來
+  if (lockBoard) return;
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
@@ -22,7 +25,7 @@ function checkForMatch() {
   // 如果牌組配對成功 => isMatch
   // 就不可以再點擊那組牌 => disableCards()
   // 配對錯誤就把該牌組蓋起來 => unflipCards()
-  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.name;
   isMatch ? disableCards() : unflipCards();
 }
 
@@ -33,10 +36,13 @@ function disableCards() {
 }
 
 function unflipCards() {
+  lockBoard = true;
+
   // 把牌蓋起來
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+    lockBoard = false;
   }, 1500);
 }
 
