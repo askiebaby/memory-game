9b7ca4b88a7e1191ad998ab43a76d1a92c365e32
Askie Lin <isme408@gmail.com>
2019-04-02 23:51:47 +0800

    added matching logic

--- script.js
+++ script.js
@@ -9,7 +9,35 @@ function flipCard() {
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this; // this => the clicked card
+    return;
   }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  // 如果牌組配對成功 => isMatch
+  // 就不可以再點擊那組牌 => disableCards()
+  // 配對錯誤就把該牌組蓋起來 => unflipCards()
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  // 移除監聽事件，釋放記憶體
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  // 把牌蓋起來
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
